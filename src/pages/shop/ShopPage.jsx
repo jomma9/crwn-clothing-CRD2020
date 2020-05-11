24a043338a38cda1e1ceb8175a6abc1cbd888f70
rajannajomma <jomma822@gmail.com>
2020-05-11 00:58:09 -0500

    collection item and collection preview

---
+++ src/pages/shop/ShopPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import CollectionPreview from "../../components/preview-collection/CollectionPreview";
+
+import "./shop-data";
+import SHOP_DATA from "./shop-data";
+
+class ShopPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
