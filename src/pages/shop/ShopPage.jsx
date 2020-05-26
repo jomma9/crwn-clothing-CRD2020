90effee6b56f756bf5b5a33352719bde744775c6
stephen jomma <stephenjomma@gmail.com>
2020-05-25 22:33:04 -0500

    applied styled components to home-page

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -3,21 +3,14 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
 import CollectionPage from "../collection/CollectionPage";
 
-const ShopPage = ({ match }) => {
-  console.log(match);
-  return (
-    <div className="shop-page">
-      <Route
-        exact
-        path={`${match.path}`}
-        component={CollectionsOverview}
-      ></Route>
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionPage}
-      ></Route>
-    </div>
-  );
-};
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview}></Route>
+    <Route
+      path={`${match.path}/:collectionId`}
+      component={CollectionPage}
+    ></Route>
+  </div>
+);
 
 export default ShopPage;
